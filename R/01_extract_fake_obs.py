69079b1c3d6c2f9ccfa843f1315787881bbb37b0
Oliver Reiter <reiter@wiiw.ac.at>
2023-07-14 13:13:47 +0200

    Add file to create fake observed data plus data

---
+++ R/01_extract_fake_obs.py
@@ -0,0 +1,26 @@
+
+import os
+import toml
+import pandas as pd
+import numpy as np
+import time
+import subprocess
+
+base_path = "/home/reitero/Arbeit/Projects/local/2023_OeNB_GeneticOptimisation_ABM/models/MultiIndustry_ABM/"
+config = toml.load(os.path.join(base_path,
+								"model_config.toml"))
+# experiment_dir = "sbi4abm_results"
+experiment_dir = "test"
+simulation_number = 1
+
+res_dt = pd.read_parquet(os.path.join(base_path,
+									  "results",
+									  experiment_dir,
+									  "sim_" + str(simulation_number),
+									  "data",
+									  "Model.parquet"),
+						 columns = ["period", "real_GDP", "unemployment_rate"])
+res_dt = res_dt[res_dt["period"] >= 1].drop(columns = ["period"])
+res_array = res_dt.to_numpy()
+
+np.savetxt("../sbi4abm/data/MultiIndustryABM/obs.txt", res_array)
