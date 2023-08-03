82863392a7bd51fa841a7f7b4867960f827e2745
Oliver Reiter <reiter@wiiw.ac.at>
2023-08-03 22:13:49 +0200

    Update test_methods

--- test_methods.sh
+++ test_methods.sh
@@ -3,9 +3,9 @@ set -u
 nsims=${1:-"100x5"}
 output_dir=${2:-"results"}
 
-for embedding in gru ##elman
+for embedding in gru elman
 do
-    for network in maf ##mdn nsf made
+    for network in maf mdn nsf made
     do
         echo "\\n=> Testing ${network}_${embedding}"
         python sbi4abm/utils/job_script.py --task mvgbm --method ${network}_${embedding} --outloc ${output_dir} --nsims ${nsims}
