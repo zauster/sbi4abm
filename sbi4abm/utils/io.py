92212e4fcdd6b147d7dae5b51986e273d4414401
Oliver Reiter <reiter@wiiw.ac.at>
2023-09-20 22:27:41 +0200

    Calibration with six variables

--- sbi4abm/utils/io.py
+++ sbi4abm/utils/io.py
@@ -83,8 +83,8 @@ def _load_prior(task_name):
 		prior = utils.BoxUniform(low=torch.tensor([-1.,-1.,-1.]),
 								 high=torch.tensor([1., 1., 1.]))
 	elif task_name == "MultiIndustryABM":
-		prior = utils.BoxUniform(low=torch.tensor([0., 0.]),
-					 high=torch.tensor([1., 0.01]))
+		prior = utils.BoxUniform(low=torch.tensor([0., 0., 0., 1., 1., 0.]),
+					 high=torch.tensor([1., 0.01, 1., 5., 2., 1.]))
 	return prior
 
 def _load_dataset(task_name):
