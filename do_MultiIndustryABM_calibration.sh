a87741b41f4f4cd60bb0db9d55884d068c04e854
Oliver Reiter <reiter@wiiw.ac.at>
2024-07-22 16:10:05 +0200

    Shell script updates

--- do_MultiIndustryABM_calibration.sh
+++ do_MultiIndustryABM_calibration.sh
@@ -4,18 +4,18 @@
 # ./kill_jc_crashed.sh > kill_jc.log &
 
 ## activates the python environment
-# source venv_sbi4abm/bin/activate
+source venv_sbi4abm/bin/activate
 
 echo "=> Running the sbi4abm algorithm"
 python sbi4abm/utils/job_script.py \
     --task=MultiIndustryABM \
     --method=resnet_gru \
     --outloc=results \
-    --nsims=10x1 \
+    --nsims=50x1 \
     --nw=10
 
 ## deactivate python environment
-# deactivate
+deactivate
 
 ## Kill the kill script:
 # cat /tmp/kill_jc_crashed.pid | xargs kill
