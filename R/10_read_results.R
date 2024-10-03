c66dd6ccdd299cb5cbc0a3252a5e9e49f8fe3cdd
Oliver Reiter <reiter@wiiw.ac.at>
2024-10-03 09:46:29 +0200

    Update the plotting R script

--- R/10_read_results.R
+++ R/10_read_results.R
@@ -5,12 +5,21 @@ setwd(here::here())
 library(ggplot2)
 library(ggthemes)
 library(configr)
+library(stringr)
+
+estimate_mode <- function(x) {
+  d <- density(x)
+  d$x[which.max(d$y)]
+}
 
 ## result_basedir <- "results_methods_500x3_8var"
 ## result_basedir <- "results_allMethods_500x5_twoVar"
 result_basedir <- "results_artificial"
+## result_basedir <- "results_empirical"
 
-result_dirs <- list.dirs(result_basedir, full.names = TRUE, recursive = FALSE)
+result_dirs <- list.dirs(result_basedir,
+                         full.names = TRUE, recursive = FALSE) |>
+  str_subset("backup", negate = TRUE)
 ## result_dirs <- result_dirs[length(result_dirs)]
 ## input_dir <- result_dirs[length(result_dirs)]
 
@@ -46,6 +55,7 @@ for(input_dir in result_dirs) {
 }
 
 res_dt <- rbindlist(result_list)
+res_dt[, table(variable)]
 
 ## truevalues_dt <- data.table(variable = c("V1", "V2"),
 ##                             variable_desc = c("expectation reaction parameter",
@@ -55,7 +65,7 @@ res_dt <- rbindlist(result_list)
 true_toml_config <- file.path("/mnt/extData3/2023_OeNB_GeneticOptimisation_ABM/models/MultiIndustry_ABM/model_config_5industries.toml")
 truevalues_toml <- read.config(true_toml_config)
 
-truevalues_dt <- data.table(variable = paste0("V", 1:6),
+truevalues_dt <- data.table(variable = paste0("V", 1:10),
                             variable_desc = c("expectation_reaction_parameter",
                                               "inflation_adj_parameter",
                                               "financial_needs_buffer_factor",
@@ -64,30 +74,53 @@ truevalues_dt <- data.table(variable = paste0("V", 1:6),
                                               "job_search_probability_employed",
                                               "budget_adj_parameter",
                                               "credit_supply_factor_assets",
-                                              "credit_supply_factor_profits"
+                                              "credit_supply_factor_profits",
+                                              "consumption_propensity_income"
                                               ),
                             true_value = c(0.1, 0.75, 1.1, 0.005, 0.33, 0.1,
-                                           0.5, 1.0, 1.0))
+                                           0.5, 1.0, 1.0, 0.953))
 
 ## truevalues_toml[truevalues_dt$variable_desc]
 
+## Merge the 'true' values
 res_dt <- merge(res_dt, truevalues_dt,
                 by = "variable")
 
+## Make the variable descriptions a little more prettier
+res_dt[, variable_desc := str_replace_all(variable_desc, "_", " ") |>
+           str_to_sentence()]
+
+## Compute mean, mode and median of the distributions
+res_dt[, mean := mean(value), by = .(variable)]
+res_dt[, mode := estimate_mode(round(value, digits = 4)), by = .(variable)]
+res_dt[, median := median(value), by = .(variable)]
 
 plot_width <- 7
 plot_height <- 13
 
-p1 <- ggplot(res_dt, aes(x = value)) +
-  geom_vline(aes(xintercept = true_value), linewidth = 1, colour = "lightgrey") +
-  geom_density(aes(colour = method), linewidth = 1) +
+p1 <- ggplot(res_dt, aes(x = value))
+if(result_basedir %like% "artificial") {
+  p1 <- p1 + geom_vline(aes(xintercept = true_value), linewidth = 1,
+                        colour = "lightgrey")
+}
+p1 <- p1 + geom_vline(aes(xintercept = mean), linewidth = 1,
+                      colour = "green")
+p1 <- p1 + geom_vline(aes(xintercept = median), linewidth = 1,
+                      colour = "orange")
+p1 <- p1 + geom_vline(aes(xintercept = mode), linewidth = 1,
+                      colour = "brown")
+p1 <- p1 + geom_density(aes(colour = method), linewidth = 1) +
   facet_wrap(~ variable_desc, scales = "free") +
   xlab("") + ylab("") +
   theme_clean() +
+  ggtitle(label = "",
+          subtitle = "True value (if applicable, grey); mean (green), median (orange), mode (brown)") +
   scale_colour_ptol()
 p1
-## ggsave(plot = p1, height = plot_width, width = plot_height,
-##        filename = paste0("plots/", result_basedir, "_hist1.png"))
+
+ggsave(plot = p1, height = plot_width, width = plot_height,
+       filename = paste0("plots/", result_basedir, "_hist1.png"))
+
 
 ## p_gru <- ggplot(res_dt[embedding == "gru", ], aes(x = value)) +
 ##   geom_vline(aes(xintercept = true_value), linewidth = 1, colour = "lightgrey") +
